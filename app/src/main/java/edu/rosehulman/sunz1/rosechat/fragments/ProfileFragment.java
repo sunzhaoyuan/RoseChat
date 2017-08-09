1192fddd2deb470574e91f5bcfdbd669af1f8ba4
sunzhaoyuan <sunzhao.cool@gmail.com>
2017-08-10 02:23:28 +0800

    use a different class to get img.

--- app/src/main/java/edu/rosehulman/sunz1/rosechat/fragments/ProfileFragment.java
+++ app/src/main/java/edu/rosehulman/sunz1/rosechat/fragments/ProfileFragment.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,7 +22,6 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 
 import edu.rosehulman.sunz1.rosechat.R;
@@ -37,9 +37,9 @@ public class ProfileFragment extends Fragment implements View.OnClickListener, G
     private TextView mEmailTxt;
     private TextView mNickNameTxt;
     private TextView mPhoneTxt;
-    private Bitmap mBitmap;
+//    private Bitmap mBitmap;
     //    private TableLayout
-    private boolean isFirstTime;
+    private boolean isFirstTime; //TODO: probably don't need this boolean
     private DatabaseReference mDBRef;
     private StorageReference mPPicStorageRef;
     private String mCurrentUID;
@@ -61,14 +61,10 @@ public class ProfileFragment extends Fragment implements View.OnClickListener, G
         mDBRef = FirebaseDatabase.getInstance().getReference().child(Constants.PATH_CONTACT);
         Query query = mDBRef.orderByChild("uid").equalTo(mCurrentUID);
         Log.d(Constants.TAG_PROFILE, "Finished the Query just now.");
-        if (query != null) {
-            Log.d(Constants.TAG_PROFILE, "the Query is not null");
-            // TODO: get Contact from FireBase
-            isFirstTime = false;
-        } else {
-            Log.d(Constants.TAG_PROFILE, "the Query is null");
+        if (query == null) { // is the first time //TODO: ERROR! key problem here.
+            Log.d(Constants.TAG_PROFILE, "the Query is null. first time meets this contact");
             // TODO: push this contact
-            isFirstTime = true;
+//            isFirstTime = true;
             HashMap<String, Boolean> friendHashmap = new HashMap<>();
             final Contact contact = new Contact(mCurrentUID, mCurrentUID, mDefaultPicUrl, friendHashmap, null, null);
             mDBRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -80,10 +76,37 @@ public class ProfileFragment extends Fragment implements View.OnClickListener, G
 
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
-                    Toast.makeText(getActivity(), "Cannot initiate current user.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getActivity(), R.string.contact_initiate_error, Toast.LENGTH_LONG).show();
+                    Log.d(Constants.TAG_PROFILE, "fail to initiate profile");
                 }
             });
         }
+        getProfileData();
+    }
+
+    private void getProfileData(){
+        Query query = mDBRef.orderByChild("uid").equalTo(mCurrentUID);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Contact contact = (Contact) dataSnapshot.getValue(); //TODO: check if it gets contact type
+                //get Profile pic
+                Glide.with(getContext())
+                        .load(contact.getProfilePicUrl())
+                        .into(mProfileImg);
+                //get Text Data
+                mNickNameTxt.setText(contact.getNickName());
+                mEmailTxt.setText(contact.getEmail());
+                mPhoneTxt.setText(contact.getPhoneNumber());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getContext(), R.string.get_contact_error, Toast.LENGTH_SHORT).show();
+                Log.d(Constants.TAG_PROFILE, "Could not get profile error.");
+            }
+        });
+
     }
 
     private void init() {
@@ -125,22 +148,22 @@ public class ProfileFragment extends Fragment implements View.OnClickListener, G
 
     @Override
     public void onPicLoaded(Bitmap bitmap) {
-        Log.d(Constants.TAG_PROFILE, "Pic Object\n" + bitmap);
-        mBitmap = bitmap;
-        mProfileImg.setImageBitmap(mBitmap);
-        mProfileImg.setDrawingCacheEnabled(true);
-        mProfileImg.buildDrawingCache();
-        Bitmap cacheBitmap = mProfileImg.getDrawingCache();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        cacheBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        byte[] data = baos.toByteArray();
-        if (isFirstTime) {
-            // not sure if it will execute onPicLoaded first or
-            // onActivityCreated first, so I chose to use this boolean
-            // TODO: load the default pic
-            Log.d(Constants.TAG_PROFILE, "First Time : Load Default");
-        } else {
-
-        }
+//        Log.d(Constants.TAG_PROFILE, "Pic Object\n" + bitmap);
+//        mBitmap = bitmap;
+//        mProfileImg.setImageBitmap(mBitmap);
+//        mProfileImg.setDrawingCacheEnabled(true);
+//        mProfileImg.buildDrawingCache();
+//        Bitmap cacheBitmap = mProfileImg.getDrawingCache();
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        cacheBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+//        byte[] data = baos.toByteArray();
+//        if (isFirstTime) {
+//            // not sure if it will execute onPicLoaded first or
+//            // onActivityCreated first, so I chose to use this boolean
+//            // TODO: load the default pic
+//            Log.d(Constants.TAG_PROFILE, "First Time : Load Default");
+//        } else {
+//
+//        }
     }
 }
