bb441804e472d974f34bcd33f80a414505733e03
sunzhaoyuan <sunzhao.cool@gmail.com>
2017-08-12 11:10:55 +0800

    fixed bug in ProfileFragment

--- app/src/main/java/edu/rosehulman/sunz1/rosechat/fragments/ProfileFragment.java
+++ app/src/main/java/edu/rosehulman/sunz1/rosechat/fragments/ProfileFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -170,16 +169,14 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         if (v.getId() == R.id.profile_edit) {
            
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
-            EditProfileFragment editProfileFragment = EditProfileFragment.newInstance(mEmailTxt.getText().toString(),
-                    mNickNameTxt.getText().toString(), mPhoneTxt.getText().toString());
-            transaction.addToBackStack("edit");
-            transaction.replace(R.id.container, editProfileFragment);
-            transaction.commit();
             Fragment editProfileFragment = EditProfileFragment.newInstance(
                     mEmailTxt.getText().toString(),
                     mNickNameTxt.getText().toString(),
                     mPhoneTxt.getText().toString()
             );
+            transaction.addToBackStack("edit");
+            transaction.replace(R.id.container, editProfileFragment);
+            transaction.commit();
         }
     }
 
